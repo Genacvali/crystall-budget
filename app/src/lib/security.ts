cb09bb0e2a0934a97d97cd868bebeee26c836987
genacvali <gena12m@gmailcom>
2025-08-29 20:04:00 +0300

    update package.json and security.ts

--- app/src/lib/security.ts
+++ app/src/lib/security.ts
@@ -1,17 +1,12 @@
-import argon2 from 'argon2';
+import { hash as a2hash, verify as a2verify } from '@node-rs/argon2';
 
 export async function hashPassword(password: string): Promise<string> {
-  return argon2.hash(password, { 
-    type: argon2.argon2id,
-    memoryCost: 2 ** 16,
-    timeCost: 3,
-    parallelism: 1,
-  });
+  return a2hash(password);
 }
 
 export async function verifyPassword(password: string, hash: string): Promise<boolean> {
   try {
-    return await argon2.verify(hash, password);
+    return await a2verify(hash, password);
   } catch (error) {
     return false;
   }
