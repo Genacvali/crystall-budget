4db14233a3b6de8b7ba59dc1f760ac63a09a9aa5
genacvali <gena12m@gmailcom>
2025-08-31 21:02:59 +0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,18 @@ const path = require('path');
 
 module.exports = {
   output: 'standalone',
-  webpack: (config) => {
+  experimental: {
+    serverComponentsExternalPackages: ['@node-rs/argon2']
+  },
+  webpack: (config, { isServer }) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    
+    if (isServer) {
+      // Исключаем native модули из webpack bundling
+      if (!config.externals) config.externals = [];
+      config.externals.push('@node-rs/argon2');
+    }
+    
     return config;
   },
 };
