33ffc463df2da783f6c0ebf16e57eedfd417f35e
Genacvali <gena12m@gmail.com>
2025-09-01 22:17:46 +0300

    delete 22 files

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AppHome() {
-  return (
-    <main className="max-w-3xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-2">Добро пожаловать 👋</h1>
-      <p className="text-gray-600">Вы авторизованы. Здесь будет дашборд.</p>
-    </main>
-  );
-}
